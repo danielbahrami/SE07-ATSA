e223d25c9daa0e3e7ad6cc6f9d97fdb9ee0ac255
Wafl <mlwellerb@gmail.com>
2023-11-24 15:53:03 +0100

    updated the production-manager

--- production_manager/src/app/page.tsx
+++ production_manager/src/app/page.tsx
@@ -1,110 +1,51 @@
-"use client";
-import api from "../logic/api";
-import { useEffect, useRef, useState } from "react";
+"use client"
+import api from '../logic/api'
+import {useEffect, useRef, useState} from "react";
 import Card from "../components/card";
+import RobotControl from '../components/robot_control';
 
 export default function Home() {
-  const [schedules, setSchedules] = useState<Schedule[]>([]);
-  const [productionLines, setProductionLines] = useState<ProductionLine[]>([]);
 
-  const scheduleNameField = useRef<HTMLInputElement>();
-  const productionLineNameField = useRef<HTMLInputElement>();
+  const [robots, setRobots] = useState<Robot[]>([])
 
   useEffect(() => {
-    api.v1.schedules.getAll().then((schs) => {
-      console.log(schs);
-      setSchedules(schs);
-    });
-    api.v1.productionLine.getAll().then((plns) => {
-      console.log(plns);
-      setProductionLines(plns);
-    });
-  }, []);
+    api.v1.robot.getAll().then(rs => {
+      setRobots(rs.sort((a,b) => comp(a,b)))
+    })
+  }, [])
 
-  const createSchedule = (event) => {
-    event.preventDefault();
-    if (!scheduleNameField.current) {
-      return;
-    }
-    const schedule: Schedule = {
-      ID: `sch_${schedules.length + 1}`,
-      Name: scheduleNameField.current!.value,
-      Description: "DESCRIPTION",
-      ProductionLineId: "prod_01",
-    };
-    console.log(schedule);
-    api.v1.schedules.post(schedule).then((r) => {
-      console.log(r);
-      schedules.push(schedule);
-      setSchedules([...schedules]);
-      console.log(schedules);
-    });
-  };
+  const comp = (a: Robot, b: Robot): number => {
+    console.log(a.ID, b.ID);
+    
+    return Number(a.ID.split("_")[1]) - Number(b.ID.split("_")[1])
+  }
 
-  const createProductionLine = (event) => {
-    event.preventDefault();
-    if (!productionLineNameField.current) {
-      return;
+  const stateColor = (state: string): string => {
+    switch (state) {
+      case "STARTING":
+        return "cyan"
+      case "RUNNING":
+        return "green"
+      case "STOPPING":
+        return "orange"
+      case "IDLE":
+        return "yellow"
+      default:
+        return "red"
     }
-    const productionLine: ProductionLine = {
-      ID: `pln_${productionLines.length + 1}`,
-      Name: productionLineNameField.current!.value,
-      Status: "Standby",
-    };
-    console.log(productionLine);
-    api.v1.productionLine.post(productionLine).then((r) => {
-      console.log(r);
-      productionLines.push(productionLine);
-      setProductionLines([...productionLines]);
-      console.log(productionLines);
-    });
-  };
+  }
 
   return (
     <main>
-      {schedules.map((sch) => (
-        <Card
-          key={`key_${sch.ID}`}
-          title={"Schedule"}
-          body={
-            <div>
-              {sch.Name} ({sch.ID})
-            </div>
-          }
-        />
-      ))}
-
-      <form onSubmit={createSchedule}>
-        <input
-          type="text"
-          name="schedule_id"
-          id="schedule_id"
-          ref={scheduleNameField}
-        />
-        <input type="submit" value="Create Sch" />
-      </form>
-
-      {productionLines.map((pln) => (
-        <Card
-          key={`key_${pln.ID}`}
-          title={"ProductionLine"}
-          body={
-            <div>
-              {pln.Name} Status: {pln.Status}
-            </div>
-          }
-        />
-      ))}
-
-      <form onSubmit={createProductionLine}>
-        <input
-          type="text"
-          name="production_line_name"
-          id="production_line_id"
-          ref={productionLineNameField}
-        />
-        <input type="submit" value="Create Pln" />
-      </form>
+      <h1>Production manager</h1>
+      {robots.map(r => 
+      <Card key={`key_${r.ID}`} title="Robot" body={
+        <div>
+          ID: {r.ID} 
+          <div className="state" style={{display: 'flex'}}>
+            <span>State: </span><div className="state-indicator" style={{background: stateColor(r.State), width: "1rem", height: "1rem", borderRadius: "50%"}}/>
+          </div> <RobotControl robot={r}/>
+        </div>}></Card>)}
     </main>
-  );
+  )
 }
