83e2b6cbea2325f5b6e29c53c33c52839e5829ef
Daniel Bahrami <daniel@bahrami.dk>
2023-12-01 13:35:41 +0100

    Remove broker from main.go

--- influxdb/main.go
+++ influxdb/main.go
@@ -7,52 +7,9 @@ import (
 	"os"
 	"time"
 
-	"../scheduling_system/env"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api/write"
 )
-
-type IBroker interface {
-	Connect() bool
-	Message(topic string, message string)
-}
-
-type Broker struct {
-	client mqtt.Client
-}
-
-func (b Broker) Connect() bool {
-	log.Println("Connecting to message broker ...")
-	brokerAddr := env.Get("BROKER")
-	options := mqtt.NewClientOptions()
-	options.AddBroker(fmt.Sprintf("tcp://%s", brokerAddr))
-	options.SetClientID("atse_scheduling_system")
-	options.OnConnect = onConnect
-	options.OnConnectionLost = onConnectionLost
-
-	client := mqtt.NewClient(options)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		log.Printf("Could not connect to message broker {%s}\n", token.Error())
-		return false
-	}
-	return true
-}
-
-func (b Broker) Message(topic string, message string) {
-	if b.client != nil {
-		token := b.client.Publish(topic, 1, false, message)
-		token.Wait()
-	}
-}
-
-func onConnect(client mqtt.Client) {
-	log.Println("Connection established")
-}
-
-func onConnectionLost(client mqtt.Client, err error) {
-	log.Printf("Connection lost {%v}\n", err)
-}
 
 func main() {
 	// Initialize client
